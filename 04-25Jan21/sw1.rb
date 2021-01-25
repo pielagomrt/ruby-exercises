4bed4eecc087a9337f10d370198c9c9eae5878a9
Regina Pielago <pielago.mrt@gmail.com>
2021-01-25 16:00:31 +0300

    update Exercise 01

--- 04-25Jan21/sw1.rb
+++ 04-25Jan21/sw1.rb
@@ -15,8 +15,8 @@ For input [0, 0, 0, 0, 0, 0, 0, 0, 0] => [0,0]
 
 def count_or_sum(*x)
   arr = [0,0]
-  if x.length <= 0
-    return arr
+  if x.length == 0 || x.nil? || x.empty?
+    return []
   end
 
   for i in 0...x.length
@@ -32,3 +32,4 @@ end
 count_or_sum 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15
 count_or_sum 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14
 count_or_sum 0, 0, 0, 0, 0, 0, 0, 0, 0
+count_or_sum
