7aa7e604abcbaa3ba8259ce3d1a291c40db70f0a
Regina Pielago <pielago.mrt@gmail.com>
2021-01-25 15:26:04 +0300

    submit Exercise 01

--- 04-25Jan21/sw1.rb
+++ 04-25Jan21/sw1.rb
@@ -7,18 +7,20 @@ If the input array is empty or null, return an empty array.
 Example
 
 For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+For input [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14] => [8,-50]
+For input [0, 0, 0, 0, 0, 0, 0, 0, 0] => [0,0]
 
 =end
 
 
 def count_or_sum(*x)
   arr = [0,0]
-  if x.length < 0
+  if x.length <= 0
     puts arr
   end
 
   for i in 0...x.length
-    if x[i] < 0
+    if x[i] <= 0
       arr[1] += x[i]
     else
       arr[0] += 1
@@ -28,3 +30,5 @@ def count_or_sum(*x)
 end
 
 count_or_sum 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15
+count_or_sum 0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14
+count_or_sum 0, 0, 0, 0, 0, 0, 0, 0, 0
