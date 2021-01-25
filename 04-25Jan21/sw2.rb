66855712b1eb8d384fc4496539c84cc995ea5813
Regina Pielago <pielago.mrt@gmail.com>
2021-01-25 20:12:53 +0300

    submit Exercise 02

---
+++ 04-25Jan21/sw2.rb
@@ -0,0 +1,131 @@
+# SOLID PRINCIPLES
+
+# Single Responsibility Principle
+class Average
+  def initialize(num_1,num_2) 
+    @num_1 = num_1
+    @num_2 = num_2
+  end
+
+  def get_average
+    ave = (@num_1 + @num_2) / 2
+    puts "Average: " + ave.to_s
+  end
+end
+
+average = Average.new(90,88).get_average
+
+
+
+# Open/closed Principle
+class Member
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Officer < Member
+  def identify
+    puts "#{@name} is an officer."
+  end
+end
+
+class Volunteer < Member
+  def identify
+    puts "#{@name} is a volunteer."
+  end
+end
+
+Jane = Officer.new("Jane").identify
+Liz = Volunteer.new("Liz").identify
+
+
+
+# Liskov Substitution Principle
+class Animal
+  def to_s # string representation of an object
+    "I\'m a pet!"
+  end
+end
+
+class Cat < Animal
+  def to_s
+    'I\'m a cat!'
+  end
+end
+
+class Dog < Animal
+  def to_s
+    'I\'m a dog!'
+  end
+end
+
+class Pet
+  def greet(animal)
+    puts animal
+  end
+end
+
+pet = Pet.new
+pet.greet(Animal.new)
+pet.greet(Cat.new)
+pet.greet(Dog.new)
+
+
+
+# Interface Segregation Principle
+class Access
+  def authorized
+    puts "Authorized Personnel"
+  end
+
+  def unauthorized
+    puts "Client/Visitor"
+  end
+end
+
+class Employee
+  def access
+    Access.new.authorized
+  end
+end
+
+class Client
+  def access
+    Access.new.unauthorized
+  end
+end
+
+supervisor = Employee.new.access
+pharma = Client.new.access
+
+
+
+# Dependency Inversion Principle
+class Cat_or_dog
+  def this_cat
+    Cat.new.this_pet
+  end
+
+  def this_dog
+    Dog.new.this_pet
+  end
+end
+
+class Cat
+  def this_pet 
+    puts "This is a cat!"
+  end
+end
+
+class Dog
+  def this_pet
+    puts "This is a dog!"
+  end
+end
+
+cat = Cat_or_dog.new.this_cat
+dog = Cat_or_dog.new.this_dog
+
+
+ 
