344475b7e9ad7c9c1ec2fa86f8d8edda2753ce88
Regina Pielago <pielago.mrt@gmail.com>
2021-01-26 14:36:39 +0300

    submit Exercise 01

--- 05-26Jan21/sw1.rb
+++ 05-26Jan21/sw1.rb
@@ -16,7 +16,7 @@ time = 11.8--> litres = 5
 def litres(*time)
   for i in 0...time.length
     ltr_dec = (0.5 * time[i]).to_f
-    puts "litres = " + (ltr_dec.to_i).to_s
+    return "litres = " + (ltr_dec.to_i).to_s
   end
 end
 
