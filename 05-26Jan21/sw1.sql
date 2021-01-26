279c491835f2ba0668c48d58ef1932bacfc297ef
Regina Pielago <pielago.mrt@gmail.com>
2021-01-26 16:37:32 +0300

    submit sql exercise

---
+++ 05-26Jan21/sw1.sql
@@ -0,0 +1,39 @@
+/*
+#1 Create new table:
+    Table name: students Columns: id , first_name , middle_name , last_name , age , location
+
+#2 Insert 6 records in your students table.
+    (1, "Juan", "Blank", "Cruz", 18, "Manila"),
+    (2, "John", "Blank", "Young", 20, "Manila"),
+    (3, "Victor", "Blank", "Rivera", 21, "Manila"),
+    (4, "Adrian", "Blank", "Co", 19, "Laguna"),
+    (5, "Pau", "Blank", "Riosa", 22, "Marikina"),
+    (6, "Piolo", "Blank", "Pascual", 25, "Manila");
+    
+#3 Update your first record: 
+    first_name : Ana middle_name : Cui last_name : Cajocson age : 25 location : Bulacan
+
+#4 Delete your last record in your students table
+*/
+
+/* #1 */
+CREATE TABLE students(id integer, first_name VARCHAR(10), middle_name VARCHAR(10), last_name VARCHAR(10), age integer, location VARCHAR(10));
+
+/* #2 */
+INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
+VALUES
+    (1, "Juan", "Blank", "Cruz", 18, "Manila"),
+    (2, "John", "Blank", "Young", 20, "Manila"),
+    (3, "Victor", "Blank", "Rivera", 21, "Manila"),
+    (4, "Adrian", "Blank", "Co", 19, "Laguna"),
+    (5, "Pau", "Blank", "Riosa", 22, "Marikina"),
+    (6, "Piolo", "Blank", "Pascual", 25, "Manila");
+
+/* #3 */    
+UPDATE students 
+SET first_name = "Ana", middle_name = "Cui", last_name = "Cajocson", age = 25, location = "Bulacan"
+WHERE id = 1;
+
+/* #4 */
+DELETE FROM students
+WHERE id = 6;
--- 05-26Jan21/sw2.sql
+++ 05-26Jan21/sw2.sql
@@ -1,17 +1,29 @@
-CREATE TABLE students(id integer, first_name VARCHAR(10), middle_name VARCHAR(10), last_name VARCHAR(10), age integer, location VARCHAR(10));
+/*
+#1 Use your previous table students
+#2 Display the count of all students
+#3 Select all students with location is Manila
+#4 Display the average age of all students
+#5 Display all students by age descending order
+*/
 
-INSERT INTO students (id, first_name, middle_name, last_name, age, location) 
-VALUES
-    (1, 'Juan', 'Blank', 'Cruz', 18, 'Manila'),
-    (2, 'John', 'Blank', 'Young', 20, 'Manila'),
-    (3, 'Victor', 'Blank', 'Rivera', 21, 'Manila'),
-    (4, 'Adrian', 'Blank', 'Co', 19, 'Laguna'),
-    (5, 'Pau', 'Blank', 'Riosa', 22, 'Marikina'),
-    (6, 'Piolo', 'Blank', 'Pascual', 25, 'Manila');
-    
-UPDATE students 
-SET first_name = "Ana", middle_name = "Cui", last_name = "Cajocson", age = 25, location = "Bulacan"
-WHERE id = 1;
-    
-DELETE FROM students
-WHERE id = 6;
+/* #1 */
+SELECT * 
+FROM students;
+
+/* #2 */
+SELECT COUNT (id)
+FROM students;
+
+/* #3 */
+SELECT *
+FROM students
+WHERE location="Manila";
+
+/* #4 */
+SELECT AVG (age)
+FROM students;
+
+/* #5 */
+SELECT *
+FROM students
+ORDER BY age DESC;
