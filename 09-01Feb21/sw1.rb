0f0db9a8dfcbbdae6804e9e5210a5c50f832f7e8
Regina Pielago <pielago.mrt@gmail.com>
2021-02-01 22:56:56 +0300

    submit exercise

--- 09-01Feb21/sw1.rb
+++ 09-01Feb21/sw1.rb
@@ -2,9 +2,11 @@
 
 Format a string of names like 'Bart, Lisa & Maggie'.
 
-Given: an array containing hashes of names Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand. 
+Given: an array containing hashes of names 
 
-Examples : 
+Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand. 
+
+Examples: 
 
 list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ]) 
   # returns 'Bart, Lisa & Maggie' 
